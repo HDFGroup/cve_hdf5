e425d3ee5fa367e8f967fd252d2c29de7e3c448e
Binh-Minh Ribler <bmribler@hdfgroup.org>
2023-04-23 16:51:17 -0500

    Preliminary testing all available CVE files with 1.10.9, 1.12.0, 1.14.0, and develop

--- test_hdf5_cve.sh
+++ test_hdf5_cve.sh
@@ -16,125 +16,177 @@
 echo "Testing HDF5 against known CVE issues"
 echo ""
 
-# I'm using cve_hdf5 currently
 #
+# this is currently cve_hdf5
 srcdir=`pwd`
-testdir=$srcdir/testfiles
-bindir=/scr/bmribler
+
+LIST_OF_BRANCHES="
+10
+12
+14
+15
+"
 
 TESTNAME=h5dump
 EXIT_SUCCESS=0
 EXIT_FAILURE=1
 nerrors=0
 
-# temporary executables (what I happen to have around),
-# need to know how we do with the different branches
 #
-DUMPER1_14=/mnt/hdf/bmribler/H5lib/jelly-1140/bin/h5dump
-DUMPER1_12=/scr/bmribler/build_hdf5-1_12_0/built/bin/h5dump
-DUMPER1_10=/scr/bmribler/build_hdf5-1_10_9/built-debug/bin/h5dump
+# currently used the versions built here
+bindir=/scr/bmribler
 
-SRC_CVE_TESTFILES=$srcdir/testfiles
+#
+# temporary executables (what I happen to have around)
+DUMPER1_15=$bindir/build_dev_branch/built/bin/h5dump
+DUMPER1_14=$bindir/build_hdf5-1_14_0/built/bin/h5dump
+DUMPER1_12=$bindir/build_hdf5-1_12_0/built/bin/h5dump
+DUMPER1_10=$bindir/build_hdf5-1_10_9/built-debug/bin/h5dump
+
+#
+# location of CVE files
+# using ../ to run tests in multiple branch directories
+CVE_TESTFILES_DIR=../testfiles
 
 LIST_CVEHDF5_TEST_FILES="
-$SRC_CVE_TESTFILES/cve-2017-17505.h5
-$SRC_CVE_TESTFILES/cve-2017-17506.h5
-$SRC_CVE_TESTFILES/cve-2017-17507.h5
-$SRC_CVE_TESTFILES/cve-2017-17508.h5
-$SRC_CVE_TESTFILES/cve-2017-17509.h5
-$SRC_CVE_TESTFILES/cve-2018-11202.h5
-$SRC_CVE_TESTFILES/cve-2018-11203.h5
-$SRC_CVE_TESTFILES/cve-2018-11204.h5
-$SRC_CVE_TESTFILES/cve-2018-11205.h5
-$SRC_CVE_TESTFILES/cve-2018-11206-new.h5
-$SRC_CVE_TESTFILES/cve-2018-11206-old.h5
-$SRC_CVE_TESTFILES/cve-2018-13873.h5
-$SRC_CVE_TESTFILES/cve-2018-15671.h5
-$SRC_CVE_TESTFILES/cve-2018-15672.h5
-$SRC_CVE_TESTFILES/cve-2018-17233.h5
-$SRC_CVE_TESTFILES/cve-2018-17234.h5
-$SRC_CVE_TESTFILES/cve-2018-17433.h5
-$SRC_CVE_TESTFILES/cve-2018-17434.h5
-$SRC_CVE_TESTFILES/cve-2018-17435.h5
-$SRC_CVE_TESTFILES/cve-2018-17436.h5
-$SRC_CVE_TESTFILES/cve-2018-17437.h5
-$SRC_CVE_TESTFILES/cve-2018-17438.h5
-$SRC_CVE_TESTFILES/cve-2018-17439.h5
-$SRC_CVE_TESTFILES/cve-2019-8396.h5
-$SRC_CVE_TESTFILES/cve-2019-8397.h5
-$SRC_CVE_TESTFILES/cve-2019-8398.h5
-$SRC_CVE_TESTFILES/cve-2019-9151.h5
-$SRC_CVE_TESTFILES/cve-2019-9152.h5
-$SRC_CVE_TESTFILES/cve-2020-10809.h5
-$SRC_CVE_TESTFILES/cve_2020_10810.h5
-$SRC_CVE_TESTFILES/cve-2020-10810.h5
-$SRC_CVE_TESTFILES/cve-2020-10811.h5
-$SRC_CVE_TESTFILES/cve_2020_10812.h5
-$SRC_CVE_TESTFILES/cve-2020-10812.h5
-$SRC_CVE_TESTFILES/cve-2021-45829.h5
-$SRC_CVE_TESTFILES/cve-2021-45830.h5
-$SRC_CVE_TESTFILES/cve-2021-45833.h5
-$SRC_CVE_TESTFILES/cve-2021-46242.h5
-$SRC_CVE_TESTFILES/cve-2021-46243.h5
-$SRC_CVE_TESTFILES/cve-2021-46244.h5
+$CVE_TESTFILES_DIR/cve-2017-17505.h5
+$CVE_TESTFILES_DIR/cve-2017-17506.h5
+$CVE_TESTFILES_DIR/cve-2017-17507.h5
+$CVE_TESTFILES_DIR/cve-2017-17508.h5
+$CVE_TESTFILES_DIR/cve-2017-17509.h5
+$CVE_TESTFILES_DIR/cve-2018-11202.h5
+$CVE_TESTFILES_DIR/cve-2018-11203.h5
+$CVE_TESTFILES_DIR/cve-2018-11204.h5
+$CVE_TESTFILES_DIR/cve-2018-11205.h5
+$CVE_TESTFILES_DIR/cve-2018-11206-new.h5
+$CVE_TESTFILES_DIR/cve-2018-11206-old.h5
+$CVE_TESTFILES_DIR/cve-2018-13873.h5
+$CVE_TESTFILES_DIR/cve-2018-15672.h5
+$CVE_TESTFILES_DIR/cve-2018-17233.h5
+$CVE_TESTFILES_DIR/cve-2018-17234.h5
+$CVE_TESTFILES_DIR/cve-2018-17433.h5
+$CVE_TESTFILES_DIR/cve-2018-17434.h5
+$CVE_TESTFILES_DIR/cve-2018-17435.h5
+$CVE_TESTFILES_DIR/cve-2018-17436.h5
+$CVE_TESTFILES_DIR/cve-2018-17437.h5
+$CVE_TESTFILES_DIR/cve-2018-17438.h5
+$CVE_TESTFILES_DIR/cve-2018-17439.h5
+$CVE_TESTFILES_DIR/cve-2019-8396.h5
+$CVE_TESTFILES_DIR/cve-2019-8397.h5
+$CVE_TESTFILES_DIR/cve-2019-8398.h5
+$CVE_TESTFILES_DIR/cve-2019-9151.h5
+$CVE_TESTFILES_DIR/cve-2019-9152.h5
+$CVE_TESTFILES_DIR/cve-2020-10809.h5
+$CVE_TESTFILES_DIR/cve_2020_10810.h5
+$CVE_TESTFILES_DIR/cve-2020-10810.h5
+$CVE_TESTFILES_DIR/cve-2020-10811.h5
+$CVE_TESTFILES_DIR/cve_2020_10812.h5
+$CVE_TESTFILES_DIR/cve-2020-10812.h5
+$CVE_TESTFILES_DIR/cve-2021-45829.h5
+$CVE_TESTFILES_DIR/cve-2021-45830.h5
+$CVE_TESTFILES_DIR/cve-2021-45833.h5
+$CVE_TESTFILES_DIR/cve-2021-46242.h5
+$CVE_TESTFILES_DIR/cve-2021-46243.h5
+$CVE_TESTFILES_DIR/cve-2021-46244.h5
 "
 
-# Print a line beginning with the word "Testing".
 #
+# files cannot be run due to infinite loop
+LIST_LEFT_OUT="
+$CVE_TESTFILES_DIR/cve-2018-15671.h5
+"
+
+#
+# print a line for the CVE file being tested on
 TESTING() {
-   echo "Testing $* "
+   echo "Dumping file $1 "
 }
 
-DUMPER_ACTUAL=$testdir/dumper_out
-
-# run h5dump on a CVE file and check for various types of crashes
 #
-DUMP_CVEFILE() {
+# run h5dump of a branch on a given CVE file and report PASSED or FAILED
+TESTDUMP_CVEFILE() {
 
-    infile=$1
-    actual="$DUMPER_ACTUAL/`basename $1 .exp`.out"
+    DUMPER=$1
+    infile=$2
+    outputdir=$3
+
+    #
+    # store actual output in a file for inspection and reducing clutter on screen
+    actual="$outputdir/`basename $2 .exp`.out"
     echo ""
 
-    # Run test
-    TESTING $TESTNAME $infile
+    # run test
+    TESTING $infile
     (
-        $DUMPER1_10 "$@" $infile
+        echo ""
+        $DUMPER $infile
     ) >&$actual
     RET=$?
-    # Segfault occurred
+
+    # FAILED when crashes occurred, PASSED means normal failure
     if [ $RET == 139 ] ; then
         nerrors="`expr $nerrors + 1`"
-        echo "*FAILED - test on $infile produced Segmentation fault (core dumped)"
-    # Floating point exception
+        echo "*FAILED - Segmentation fault (core dumped)"
     elif [ $RET == 136 ] ; then
         nerrors="`expr $nerrors + 1`"
-        echo "*FAILED - test on $infile produced Floating point exception(core dumped)"
-    # Aborted
+        echo "*FAILED - Floating point exception(core dumped)"
     elif [ $RET == 134 ] ; then
         nerrors="`expr $nerrors + 1`"
-        echo "*FAILED - test on $infile got Aborted (core dumped)"
+        echo "*FAILED - Aborted (core dumped)"
     else
         echo " PASSED"
     fi
+
 }
 
-# create directory to store actual output
 #
-mkdir -p -m=777 $DUMPER_ACTUAL
-
-# run h5dump on each CVE file
-#
-for testfile in $LIST_CVEHDF5_TEST_FILES
+# for each branch, create test and test output directories, then run the
+# test using TESTDUMP_CVEFILE, passing in h5dump of that branch, the CVE
+# file, and the test output directory
+for branch in $LIST_OF_BRANCHES
 do
-    DUMP_CVEFILE $testfile
+    echo ""
+    echo "==================================================================="
+    echo "                      Testing in 1.$branch"
+    echo "==================================================================="
+
+    # create top directory for each branch
+    testdir=test1_$branch
+    mkdir -p -m=755 $testdir
+    cd $testdir
+
+    if [ $branch == 10 ] ; then
+        DUMPER=$DUMPER1_10
+    elif [ $branch == 12 ] ; then
+        DUMPER=$DUMPER1_12
+    elif [ $branch == 14 ] ; then
+        DUMPER=$DUMPER1_14
+    elif [ $branch == 15 ] ; then
+        DUMPER=$DUMPER1_15
+    else
+        echo "only branches 1.10, 1.12, and 1.14 available"
+    fi
+
+    # create directory to store actual output in each branch
+    output_testdir=dumper_out
+    mkdir -p -m=755 $output_testdir
+
+    # run h5dump on each CVE file
+    for testfile in $LIST_CVEHDF5_TEST_FILES
+    do
+        TESTDUMP_CVEFILE $DUMPER $testfile $output_testdir
+    done
+    cd ..
 done
 
+#
 # cleanup actual output
-#
+echo ""
 echo "*** not cleanup the output files at this time, they should be inspected ***"
+echo ""
 
-# Report test results and exit
 #
+# Report test results and exit
 if test $nerrors -eq 0 ; then
     echo "All $TESTNAME tests passed."
     exit $EXIT_SUCCESS
